4cb4318ecb142833e9adfbb303c536a97f205c29
Roy Zhang <roy.zhang@nike.com>
2019-06-24 01:15:45 -0700

    constrained player movement boundaries

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -42,19 +42,28 @@
 
 (.. (local-position (object-named "empty")) y)
 
+;; the closer to 2, the less it gets advanced
+(defn constrained-movement [current-val going-further?]
+  (if going-further? ; meaning outside and going further
+    (* (- 2 (abs current-val)) (if (pos? current-val) 0.05 -0.05))
+    (if (pos? current-val)
+      -0.1
+      0.1)))
+
 (defn handle-input [obj k]
   (when (input/key? "q")
     (hard.core/rotate! obj (l/v3 0 -6 0)))
   (when (input/key? "e")
     (hard.core/rotate! obj (l/v3 0 6 0)))
+
   (when (input/key? "a")
-    (move! obj (l/v3 -0.1 0 0)))
+    (move! obj (l/v3 (constrained-movement (X obj) (neg? (X obj))) 0 0)))
   (when (input/key? "d")
-    (move! obj (l/v3 0.1 0 0)))
+    (move! obj (l/v3 (constrained-movement (X obj) (pos? (X obj))) 0 0)))
   (when (input/key? "w")
-    (move! obj (l/v3 0 0 -0.1)))
+    (move! obj (l/v3 0 0 (constrained-movement (Z obj) (neg? (Z obj))))))
   (when (input/key? "s")
-    (move! obj (l/v3 0 0 0.1))))
+    (move! obj (l/v3 0 0 (constrained-movement (Z obj) (pos? (Z obj)))))))
 
 (defn player-collision-fn [obj role-key collision]
   (log collision))
@@ -63,17 +72,14 @@
   (hard.core/clear-cloned!)
   (hard.core/clone! :camera)
   (hard.core/clone! :sun)
+  ;(hard.core/clone! :hell-sun)
   (hard.core/clone! :tube)
+
   (let [player (hard.core/clone! :player)
         head (first (children player))]
+    (hook- head :on-trigger-enter :player-collision)
+    (hook- player :update :handle-input)
     (hook+ head :on-trigger-enter :player-collision player-collision-fn)
     (hook+ player :update :handle-input handle-input)))
-
-(hook- (object-named "player") :update :handle-input)
-(hard.core/clone! :segment)
-(hard.core/clear-cloned!)
-
-(def big-tube (create-primitive :cylinder))
-
 
 (start-game nil)
--- Assets/game/obstacles.clj
+++ Assets/game/obstacles.clj
@@ -1,7 +1,7 @@
 (ns game.obstacles
   (:use arcadia.core arcadia.linear hard.core tween.core))
 
-(def y-to-retire -5)
+(def y-to-retire -3)
 
 (defn ring-update [obj k]
   (when (> y-to-retire (Y obj))
@@ -21,6 +21,8 @@
   (let [pat (repeatedly 6 #(< 5 (rand-int 10)))]
     (make-ring pat)))
 
+(make-random-ring nil nil)
+
 (defn start-dropping-rings! [intensity]
   (timeline* :loop
              (wait intensity)
@@ -29,11 +31,15 @@
 ;; secs between drops
 (start-dropping-rings! 0.5)
 
-
-
-(defn make-wall [])
-
-
+(defn make-random-wall [pat]
+  (dorun
+    (map-indexed
+      (fn [i s]
+        (if (= s true)
+          (let [segment (clone! :wall-plank)]
+            (rotate! segment (v3 0 (* i 60) 0))
+            (hook+ segment :update :ring-update ring-update))))
+      pat)))
 
 
 ;; stop dropping stuff
